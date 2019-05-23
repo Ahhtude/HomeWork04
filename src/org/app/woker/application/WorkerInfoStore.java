aa61ef9a54ad583714454ffb69c92baf204ff1f9
sergey <whteahhtude@gmail.com>
2019-05-23 14:34:00 +0300

    Выполнение домашнего задания 4

--- src/org/app/woker/application/WorkerInfoStore.java
+++ src/org/app/woker/application/WorkerInfoStore.java
@@ -5,19 +5,23 @@ import org.app.woker.entities.Programmer;
 import org.app.woker.entities.QAEngineer;
 import org.app.woker.entities.Worker;
 
+import java.util.Arrays;
+
 
 public final class WorkerInfoStore {
 
     private static WorkerInfoStore instance = new WorkerInfoStore();
-    private WorkerInfoStore(){
+
+    private WorkerInfoStore() {
     }
-    public static WorkerInfoStore getInstance(){
+
+    public static WorkerInfoStore getInstance() {
         return instance;
     }
 
     private static final int initialSizeForArrays = 10;
 
-    private Manager [] managers = new Manager [initialSizeForArrays];
+    private Manager[] managers = new Manager[initialSizeForArrays];
     private Programmer[] programmers = new Programmer[initialSizeForArrays];
     private QAEngineer[] qaEngineers = new QAEngineer[initialSizeForArrays];
 
@@ -25,37 +29,51 @@ public final class WorkerInfoStore {
     private int programmersCount;
     private int qaEngineersCount;
 
-    public void addMeneger(Manager manager){
-        if (managers.length <= managersCount){
+    public void addMeneger(Manager manager) {
+        if (managers.length <= managersCount) {
+            managers[managersCount] = manager;
+            managersCount += 1;
+        } else {
+            managers = Arrays.copyOf(managers, managers.length + 10);
             managers[managersCount] = manager;
             managersCount += 1;
         }
     }
 
-    public void addProgrammer(Programmer programmer){
-        if (programmers.length <= programmersCount){
+    public void addProgrammer(Programmer programmer) {
+        if (programmers.length <= programmersCount) {
             programmers[programmersCount] = programmer;
-            programmersCount += 1 ;
+            programmersCount += 1;
+        } else {
+            programmers = Arrays.copyOf(programmers, programmers.length + 10);
+            programmers[programmersCount] = programmer;
+            programmersCount += 1;
         }
 
     }
-    public void addQAengineers(QAEngineer qaEngineer){
-        if (qaEngineers.length <= qaEngineersCount){
+
+    public void addQAengineers(QAEngineer qaEngineer) {
+        if (qaEngineers.length <= qaEngineersCount) {
+            qaEngineers[qaEngineersCount] = qaEngineer;
+            qaEngineersCount += 1;
+        } else {
+            qaEngineers = Arrays.copyOf(qaEngineers, qaEngineers.length + 10);
             qaEngineers[qaEngineersCount] = qaEngineer;
             qaEngineersCount += 1;
         }
     }
-    public void addWorker (Worker worker){
-        if (worker instanceof Manager){
+
+    public void addWorker(Worker worker) {
+        if (worker instanceof Manager) {
             addMeneger((Manager) worker);
-        }
-        else if (worker instanceof Programmer){
+        } else if (worker instanceof Programmer) {
             addProgrammer((Programmer) worker);
-        }
-        else if (worker instanceof QAEngineer){
+        } else if (worker instanceof QAEngineer) {
             addQAengineers((QAEngineer) worker);
         }
     }
 
-
+    public int getManagersCount() {
+        return managersCount;
+    }
 }
